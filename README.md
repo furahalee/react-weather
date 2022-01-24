1f81782e1bc674f3e22f4fc3556019524a3eccdf
Heemin <viridian135@gmail.com>
2022-01-24 19:57:41 +0900

    날씨검색 앱

---
+++ README.md
@@ -0,0 +1,24 @@
+## WEATHER TODAY APP
+
+전세계의 날씨를 검색하는 앱입니다.
+
+root프로젝트 폴더에 '.env' file 을 생성 후 다음의 api키를 입력하세요: (https://openweathermap.org/api) api key:
+```
+REACT_APP_WEATHER_API_KEY=your-api-key
+```
+다음의 명령어로 패키지를 인스톨하세요
+```
+npm i
+```
+development mode에서 브라우저에서 화면을 보려면 다음 로컬호스트 오픈<br />
+ [http://localhost:3000](http://localhost:3000)
+```
+npm start
+``` 
+빌드하기
+
+```
+npm run build
+```
+
+Author: @heeminlee
---
+++ src/serviceWorker.js
@@ -0,0 +1,111 @@
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] 는 IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 는 localhost IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+export function register(config) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    // 이 URL constructor모든 브라우저에서 사용가능
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      return;
+    }
+
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl, config);
+
+        navigator.serviceWorker.ready.then(() => {
+          console.log(
+            '이 앱은 서비스 워커에 의해 구동됩니다.'
+          );
+        });
+      } else {
+        registerValidSW(swUrl, config);
+      }
+    });
+  }
+}
+
+function registerValidSW(swUrl, config) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing;
+        if (installingWorker == null) {
+          return;
+        }
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              console.log(
+                '탭이 꺼진 경우 새로운 컨텐트를 볼 수 있음'
+              );
+
+              // 콜백
+              if (config && config.onUpdate) {
+                config.onUpdate(registration);
+              }
+            } else {
+              console.log('컨텐트 캐쉬에 저장.');
+
+              // 콜백
+              if (config && config.onSuccess) {
+                config.onSuccess(registration);
+              }
+            }
+          }
+        };
+      };
+    })
+    .catch(error => {
+      console.error('서비스 워커 등록중 에러가 났습니다.:', error);
+    });
+}
+
+function checkValidServiceWorker(swUrl, config) {
+  fetch(swUrl, {
+    headers: { 'Service-Worker': 'script' }
+  })
+    .then(response => {
+      const contentType = response.headers.get('content-type');
+      if (
+        response.status === 404 ||
+        (contentType != null && contentType.indexOf('javascript') === -1)
+      ) {
+        navigator.serviceWorker.ready.then(registration => {
+          registration.unregister().then(() => {
+            window.location.reload();
+          });
+        });
+      } else {
+        registerValidSW(swUrl, config);
+      }
+    })
+    .catch(() => {
+      console.log(
+        '인터넷이 연결되어 있지 않습니다.'
+      );
+    });
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then(registration => {
+        registration.unregister();
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }
+}
